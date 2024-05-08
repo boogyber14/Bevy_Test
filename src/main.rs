5c28443bde90af6e4db2a1027f33e31e5b6e535f
boogyber14 <cequina30@gmail.com>
2024-05-09 07:37:27 +0800

    Hello Josh, this is the Bevy I made as a test, but I'm not sure if it's working. Please comment below if it ran or tell me what I should do to fix any errors.

---
+++ src/main.rs
@@ -0,0 +1,29 @@
+use bevy::prelude::*;
+use bevy::prelude::Camera3dBundle;
+
+fn main() {
+    App::new().add_plugins(DefaultPlugins).add_systems(Startup, (spawn_floor, spawn_camera)).run();
+}
+
+fn spawn_camera(mut commands: Commands) {
+    let camera = Camera3dBundle {
+        transform: Transform::from_xyz(-2.0, 2.5, 5.0).looking_at(Vec3::ZERO, Vec3::Y),
+        ..default()
+    };
+
+    commands.spawn(camera);
+}
+fn spawn_floor (
+    mut commands: Commands,
+    mut meshes: ResMut<Assets<Mesh>>,
+    mut materials: ResMut<Assets<StandardMaterial>>,
+) {
+    let floor = PbrBundle {
+        mesh: meshes.add(Mesh::from(shape::Plane::from_size(15.0))),
+        material: materials.add(Color::DARK_GREEN.into()),
+        ..default()
+    };
+
+    commands.spawn(floor);
+}
+
